1bbe9c38591aded29beb2fe1903415f08ef889ca
holynacll <alexandrecurylima@hotmail.com>
2023-04-11 09:53:23 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+import { useCartStore } from "@/stores/CartStore.js"
+import { useProductStore } from "./stores/ProductStore.js";
+const cartStore = useCartStore();
+const productStore = useProductStore();
+productStore.fill();
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard
-        v-for="product in products"
-        :key="product.name"
-        :product="product"
-      />
+      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
+        @addToCart="cartStore.addItems($event, product)" />
     </ul>
   </div>
 </template>
